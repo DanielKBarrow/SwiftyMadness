a707c32291d9ce8b5c8361e589ab689b48e0621c
Daniel BARROW <dbarrow@e4r6p7.wethinkcode.co.za>
2018-10-26 12:11:47 +0200

    Jobs done

---
+++ Swifty Companion/SkillController.swift
@@ -0,0 +1,51 @@
+//
+//  SkillController.swift
+//  Swifty Companion
+//
+//  Created by Daniel BARROW on 2018/10/26.
+//  Copyright © 2018 Patrick RUSSELL. All rights reserved.
+//
+
+import UIKit
+
+class SkillController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return UserDataModel.skills.count
+    }
+    
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style : UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.adjustsFontSizeToFitWidth = true
+        let skill = UserDataModel.skills[indexPath.row]
+        cell.textLabel?.text = "\(skill.name): \(skill.percent)"
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
