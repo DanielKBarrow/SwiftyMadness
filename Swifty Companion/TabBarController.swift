8745778f5e4d0ec76a40cc8c0c6ea4793432b522
Daniel BARROW <dbarrow@e4r6p7.wethinkcode.co.za>
2018-10-30 13:08:58 +0200

    Ruinsed

--- Swifty Companion/TabBarController.swift
+++ Swifty Companion/TabBarController.swift
@@ -1,9 +1,35 @@
 //
 //  TabBarController.swift
-//  Swifty Companion
+//  
 //
 //  Created by Daniel BARROW on 2018/10/25.
-//  Copyright © 2018 Patrick RUSSELL. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    var searchUsername : String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
